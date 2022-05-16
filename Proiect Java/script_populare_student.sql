916a664ddedcf87644b0d2aca2de9db44943b844
Ana Maria Mitrea <72494328+AnaMitrea@users.noreply.github.com>
2022-05-16 12:50:13 +0300

    Script de generare automata a 1025 studenti

---
+++ Proiect Java/script_populare_student.sql
@@ -0,0 +1,104 @@
+CREATE TABLE studenti_java (
+  id INT NOT NULL PRIMARY KEY,
+  nr_matricol VARCHAR2(6) NOT NULL,
+  nume VARCHAR2(15) NOT NULL,
+  prenume VARCHAR2(30) NOT NULL,
+  gen VARCHAR2(2) NOT NULL,
+  an NUMBER(1),
+  grupa CHAR(2),
+  data_nastere DATE,
+  email VARCHAR2(40),
+/
+
+SET SERVEROUTPUT ON;
+DECLARE
+  TYPE varr IS VARRAY(1000) OF varchar2(255);
+  lista_nume varr := varr('Ababei','Acasandrei','Adascalitei','Afanasie','Agafitei','Agape','Aioanei','Alexandrescu','Alexandru','Alexe','Alexii','Amarghioalei','Ambroci','Andonesei','Andrei','Andrian','Andrici','Andronic','Andros','Anghelina','Anita','Antochi','Antonie','Apetrei','Apostol','Arhip','Arhire','Arteni','Arvinte','Asaftei','Asofiei','Aungurenci','Avadanei','Avram','Babei','Baciu','Baetu','Balan','Balica','Banu','Barbieru','Barzu','Bazgan','Bejan','Bejenaru','Belcescu','Belciuganu','Benchea','Bilan','Birsanu','Bivol','Bizu','Boca','Bodnar','Boistean','Borcan','Bordeianu','Botezatu','Bradea','Braescu','Budaca','Bulai','Bulbuc-aioanei','Burlacu','Burloiu','Bursuc','Butacu','Bute','Buza','Calancea','Calinescu','Capusneanu','Caraiman','Carbune','Carp','Catana','Catiru','Catonoiu','Cazacu','Cazamir','Cebere','Cehan','Cernescu','Chelaru','Chelmu','Chelmus','Chibici','Chicos','Chilaboc','Chile','Chiriac','Chirila','Chistol','Chitic','Chmilevski','Cimpoesu','Ciobanu','Ciobotaru','Ciocoiu','Ciofu','Ciornei','Citea','Ciucanu','Clatinici','Clim','Cobuz','Coca','Cojocariu','Cojocaru','Condurache','Corciu','Corduneanu','Corfu','Corneanu','Corodescu','Coseru','Cosnita','Costan','Covatariu','Cozma','Cozmiuc','Craciunas','Crainiceanu','Creanga','Cretu','Cristea','Crucerescu','Cumpata','Curca','Cusmuliuc','Damian','Damoc','Daneliuc','Daniel','Danila','Darie','Dascalescu','Dascalu','Diaconu','Dima','Dimache','Dinu','Dobos','Dochitei','Dochitoiu','Dodan','Dogaru','Domnaru','Dorneanu','Dragan','Dragoman','Dragomir','Dragomirescu','Duceac','Dudau','Durnea','Edu','Eduard','Eusebiu','Fedeles','Ferestraoaru','Filibiu','Filimon','Filip','Florescu','Folvaiter','Frumosu','Frunza','Galatanu','Gavrilita','Gavriliuc','Gavrilovici','Gherase','Gherca','Ghergu','Gherman','Ghibirdic','Giosanu','Gitlan','Giurgila','Glodeanu','Goldan','Gorgan','Grama','Grigore','Grigoriu','Grosu','Grozavu','Gurau','Haba','Harabula','Hardon','Harpa','Herdes','Herscovici','Hociung','Hodoreanu','Hostiuc','Huma','Hutanu','Huzum','Iacob','Iacobuta','Iancu','Ichim','Iftimesei','Ilie','Insuratelu','Ionesei','Ionesi','Ionita','Iordache','Iordache-tiroiu','Iordan','Iosub','Iovu','Irimia','Ivascu','Jecu','Jitariuc','Jitca','Joldescu','Juravle','Larion','Lates','Latu','Lazar','Leleu','Leon','Leonte','Leuciuc','Leustean','Luca','Lucaci','Lucasi','Luncasu','Lungeanu','Lungu','Lupascu','Lupu','Macariu','Macoveschi','Maftei','Maganu','Mangalagiu','Manolache','Manole','Marcu','Marinov','Martinas','Marton','Mataca','Matcovici','Matei','Maties','Matrana','Maxim','Mazareanu','Mazilu','Mazur','Melniciuc-puica','Micu','Mihaela','Mihai','Mihaila','Mihailescu','Mihalachi','Mihalcea','Mihociu','Milut','Minea','Minghel','Minuti','Miron','Mitan','Moisa','Moniry-abyaneh','Morarescu','Morosanu','Moscu','Motrescu','Motroi','Munteanu','Murarasu','Musca','Mutescu','Nastaca','Nechita','Neghina','Negrus','Negruser','Negrutu','Nemtoc','Netedu','Nica','Nicu','Oana','Olanuta','Olarasu','Olariu','Olaru','Onu','Opariuc','Oprea','Ostafe','Otrocol','Palihovici','Pantiru','Pantiruc','Paparuz','Pascaru','Patachi','Patras','Patriche','Perciun','Perju','Petcu','Pila','Pintilie','Piriu','Platon','Plugariu','Podaru','Poenariu','Pojar','Popa','Popescu','Popovici','Poputoaia','Postolache','Predoaia','Prisecaru','Procop','Prodan','Puiu','Purice','Rachieru','Razvan','Reut','Riscanu','Riza','Robu','Roman','Romanescu','Romaniuc','Rosca','Rusu','Samson','Sandu','Sandulache','Sava','Savescu','Schifirnet','Scortanu','Scurtu','Sfarghiu','Silitra','Simiganoschi','Simion','Simionescu','Simionesei','Simon','Sitaru','Sleghel','Sofian','Soficu','Sparhat','Spiridon','Stan','Stavarache','Stefan','Stefanita','Stingaciu','Stiufliuc','Stoian','Stoica','Stoleru','Stolniceanu','Stolnicu','Strainu','Strimtu','Suhani','Tabusca','Talif','Tanasa','Teclici','Teodorescu','Tesu','Tifrea','Timofte','Tincu','Tirpescu','Toader','Tofan','Toma','Toncu','Trifan','Tudosa','Tudose','Tuduri','Tuiu','Turcu','Ulinici','Unghianu','Ungureanu','Ursache','Ursachi','Urse','Ursu','Varlan','Varteniuc','Varvaroi','Vasilache','Vasiliu','Ventaniuc','Vicol','Vidru','Vinatoru','Vlad','Voaides','Vrabie','Vulpescu','Zamosteanu','Zazuleac');
+  lista_prenume_fete varr := varr('Adina','Alexandra','Alina','Ana','Anca','Anda','Andra','Andreea','Andreia','Antonia','Bianca','Camelia','Claudia','Codrina','Cristina','Daniela','Daria','Delia','Denisa','Diana','Ecaterina','Elena','Eleonora','Elisa','Ema','Emanuela','Emma','Gabriela','Georgiana','Ileana','Ilona','Ioana','Iolanda','Irina','Iulia','Iuliana','Larisa','Laura','Loredana','Madalina','Malina','Manuela','Maria','Mihaela','Mirela','Monica','Oana','Paula','Petruta','Raluca','Sabina','Sanziana','Simina','Simona','Stefana','Stefania','Tamara','Teodora','Theodora','Vasilica','Xena');
+  lista_prenume_baieti varr := varr('Adrian','Alex','Alexandru','Alin','Andreas','Andrei','Aurelian','Beniamin','Bogdan','Camil','Catalin','Cezar','Ciprian','Claudiu','Codrin','Constantin','Corneliu','Cosmin','Costel','Cristian','Damian','Dan','Daniel','Danut','Darius','Denise','Dimitrie','Dorian','Dorin','Dragos','Dumitru','Eduard','Elvis','Emil','Ervin','Eugen','Eusebiu','Fabian','Filip','Florian','Florin','Gabriel','George','Gheorghe','Giani','Giulio','Iaroslav','Ilie','Ioan','Ion','Ionel','Ionut','Iosif','Irinel','Iulian','Iustin','Laurentiu','Liviu','Lucian','Marian','Marius','Matei','Mihai','Mihail','Nicolae','Nicu','Nicusor','Octavian','Ovidiu','Paul','Petru','Petrut','Radu','Rares','Razvan','Richard','Robert','Roland','Rolland','Romanescu','Sabin','Samuel','Sebastian','Sergiu','Silviu','Stefan','Teodor','Teofil','Theodor','Tudor','Vadim','Valentin','Valeriu','Vasile','Victor','Vlad','Vladimir','Vladut');
+      
+  v_nume VARCHAR2(255);
+  v_prenume VARCHAR2(255);
+  v_prenume1 VARCHAR2(255);
+  v_prenume2 VARCHAR2(255);
+  v_matr VARCHAR2(6);
+  v_matr_aux VARCHAR2(6);
+  v_temp int;
+  v_temp1 int;
+  v_temp2 int;
+  v_temp3 int;
+  v_temp_date date;
+  v_an int;
+  v_grupa varchar2(2);
+  v_bursa int;
+  v_data_nastere date;  
+  v_email varchar2(40);
+  v_gen	varchar2(2);
+BEGIN
+  
+   DBMS_OUTPUT.PUT_LINE('Inserarea a 1025 studenti...');
+   FOR v_i IN 1..1025 LOOP
+      --SELECT SUBSTR(NAME, INSTR(NAME,' ')) INTO v_nume FROM (SELECT * FROM USERS WHERE INSTR(NAME,' ')>1 ORDER BY DBMS_RANDOM.RANDOM) WHERE ROWNUM=1;
+      --IF length(v_nume)>20 then v_nume:=substr(v_nume,1,20); end if;
+      v_nume := lista_nume(TRUNC(DBMS_RANDOM.VALUE(0,lista_nume.count))+1);
+      IF (DBMS_RANDOM.VALUE(0,100)<50) THEN 
+		 v_gen := 'F';
+         v_prenume1 := lista_prenume_fete(TRUNC(DBMS_RANDOM.VALUE(0,lista_prenume_fete.count))+1);
+         LOOP
+            v_prenume2 := lista_prenume_fete(TRUNC(DBMS_RANDOM.VALUE(0,lista_prenume_fete.count))+1);
+            exit when v_prenume1<>v_prenume2;
+         END LOOP;
+       ELSE
+		 v_gen := 'M';
+         v_prenume1 := lista_prenume_baieti(TRUNC(DBMS_RANDOM.VALUE(0,lista_prenume_baieti.count))+1);
+         LOOP
+            v_prenume2 := lista_prenume_baieti(TRUNC(DBMS_RANDOM.VALUE(0,lista_prenume_baieti.count))+1);
+            exit when v_prenume1<>v_prenume2;
+         END LOOP;       
+       END IF;
+     
+     IF (DBMS_RANDOM.VALUE(0,100)<60) THEN  
+        IF LENGTH(v_prenume1 || ' ' || v_prenume2) <= 20 THEN
+          v_prenume := v_prenume1 || ' ' || v_prenume2;
+        END IF;
+        else 
+           v_prenume:=v_prenume1;
+     END IF;       
+       
+      LOOP
+         v_matr := FLOOR(DBMS_RANDOM.VALUE(100,999)) || CHR(FLOOR(DBMS_RANDOM.VALUE(65,91))) || CHR(FLOOR(DBMS_RANDOM.VALUE(65,91))) || FLOOR(DBMS_RANDOM.VALUE(0,9));
+         select count(*) into v_temp from studenti_java where nr_matricol = v_matr;
+         exit when v_temp=0;
+      END LOOP;
+              
+      LOOP      
+        v_an := TRUNC(DBMS_RANDOM.VALUE(0,3))+1;
+        v_grupa := chr(TRUNC(DBMS_RANDOM.VALUE(0,2))+65) || chr(TRUNC(DBMS_RANDOM.VALUE(0,6))+49);
+        select count(*) into v_temp from studenti_java where an=v_an and grupa=v_grupa;
+        exit when v_temp < 30;
+      END LOOP;
+      
+      v_bursa := '';
+      IF (DBMS_RANDOM.VALUE(0,100)<10) THEN
+         v_bursa := TRUNC(DBMS_RANDOM.VALUE(0,10))*100 + 500;
+      END IF;
+      
+      v_data_nastere := TO_DATE('01-01-1974','MM-DD-YYYY')+TRUNC(DBMS_RANDOM.VALUE(0,365));
+      
+      v_temp:='';
+      v_email := lower(v_nume ||'.'|| v_prenume1);
+      LOOP         
+         select count(*) into v_temp from studenti_java where email = v_email||v_temp;
+         exit when v_temp=0;
+         v_temp :=  TRUNC(DBMS_RANDOM.VALUE(0,100));
+      END LOOP;    
+      
+      if (TRUNC(DBMS_RANDOM.VALUE(0,2))=0) then v_email := v_email ||'@gmail.com';
+         else v_email := v_email ||'@info.ro';
+      end if;
+                      
+      --DBMS_OUTPUT.PUT_LINE (v_i||' '||v_matr||' '||v_nume||' '||v_prenume ||' '|| v_an ||' '|| v_grupa||' '|| v_bursa||' '|| to_char(v_data_nastere, 'DD-MM-YYYY')||' '|| v_email);      
+      insert into studenti_java values(v_i, v_matr, v_nume, v_prenume, v_gen, v_an, v_grupa, v_bursa, v_data_nastere, v_email, sysdate, sysdate);
+   END LOOP;
+   DBMS_OUTPUT.PUT_LINE('Inserarea a 1025 studenti... GATA !');
+END;
